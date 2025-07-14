5676a0395f6a64d3b87713565d11e923961194bb
NotAlex575 <dapontealessandro89gmail.com>
2025-07-14 18:50:37 +0200

    inizio snack 2: creato l'oggetto squadre con l'implemento esterno dei nomi, aggiunti i valori punti fatti e falli fatti

---
+++ scripts/snack2.js
@@ -0,0 +1,40 @@
+let nomi_squadra = [];
+for(let i = 0; i < 5; i++){
+    nomi_squadra[i] = prompt("inserisci il nome della ",i+1," squadra: ")
+}
+
+const squadre = [
+    {
+        nome: nomi_squadra[0],
+        punti_fatti: 0,
+        falli_fatti: 0
+    },
+    {
+        nome: nomi_squadra[1],
+        punti_fatti: 0,
+        falli_fatti: 0
+    },
+    {
+        nome: nomi_squadra[2],
+        punti_fatti: 0,
+        falli_fatti: 0
+    },
+    {
+        nome: nomi_squadra[3],
+        punti_fatti: 0,
+        falli_fatti: 0
+    },
+    {
+        nome: nomi_squadra[4],
+        punti_fatti: 0,
+        falli_fatti: 0
+    }
+]
+
+for(let squadra in squadre){
+    squadra.punti_fatti = Math.floor(Math.random()*10)
+    squadra.falli_fatti = Math.floor(Math.random()*5)
+}
+
+
+
