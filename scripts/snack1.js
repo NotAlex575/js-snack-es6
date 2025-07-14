96e85102c341c989bc403307ad00c19f39118cda
NotAlex575 <dapontealessandro89gmail.com>
2025-07-14 19:23:40 +0200

    EXTRA MIO: aggiunto lato visivo esercizio snack 1

--- scripts/snack1.js
+++ scripts/snack1.js
@@ -41,3 +41,34 @@ for(let i = 1; i < biciclette.length; i++){
 }
 
 console.log("La più pesante è:", piu_pesante);
+
+//________________________________________________________________________________________________________________________________________________________________________
+
+//EXTRA MIO
+
+nome_bicicletta1 = document.getElementById("nome-bicicletta1");
+nome_bicicletta1.innerHTML = biciclette[0].nome;
+nome_bicicletta2 = document.getElementById("nome-bicicletta2");
+nome_bicicletta2.innerHTML = biciclette[1].nome;
+nome_bicicletta3 = document.getElementById("nome-bicicletta3");
+nome_bicicletta3.innerHTML = biciclette[2].nome;
+nome_bicicletta4 = document.getElementById("nome-bicicletta4");
+nome_bicicletta4.innerHTML = biciclette[3].nome;
+nome_bicicletta5 = document.getElementById("nome-bicicletta5");
+nome_bicicletta5.innerHTML = biciclette[4].nome;
+nome_bicicletta6 = document.getElementById("nome-bicicletta6");
+nome_bicicletta6.innerHTML = biciclette[5].nome;
+
+peso_bicicletta_1 = document.getElementById("peso-bicicletta-1");
+peso_bicicletta_1.innerHTML = biciclette[0].peso;
+peso_bicicletta_2 = document.getElementById("peso-bicicletta-2");
+peso_bicicletta_2.innerHTML = biciclette[1].peso;
+peso_bicicletta_3 = document.getElementById("peso-bicicletta-3");
+peso_bicicletta_3.innerHTML = biciclette[2].peso;
+peso_bicicletta_4 = document.getElementById("peso-bicicletta-4");
+peso_bicicletta_4.innerHTML = biciclette[3].peso;
+peso_bicicletta_5 = document.getElementById("peso-bicicletta-5");
+peso_bicicletta_5.innerHTML = biciclette[4].peso;
+peso_bicicletta_6 = document.getElementById("peso-bicicletta-6");
+peso_bicicletta_6.innerHTML = biciclette[5].peso;
+
